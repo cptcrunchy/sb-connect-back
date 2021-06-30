9da90ef44276c3d43db03c77f0676e678d49ca44
Jason Gutierrez <jasong84@gmail.com>
2021-06-30 17:45:18 -0500

    Update changes to strapi to use dynamic db connection string

--- config/database.js
+++ config/database.js
@@ -1,5 +1,4 @@
 module.exports = ({ env }) => {
-  if(env('NODE_ENV') === "development"){
     return {
       defaultConnection: 'default',
       connections: {
@@ -14,29 +13,5 @@ module.exports = ({ env }) => {
           },
         },
       },
-    }
-  } else {
-    return {
-      defaultConnection: 'default',
-      connections: {
-        default: {
-          connector: 'bookshelf',
-          settings: {
-            client: 'postgres',
-            host: env('DATABASE_HOST', '127.0.0.1'),
-            port: env('DATABASE_PORT', 27017),
-            database: env('DATABASE_NAME', 'strapi'),
-            user: env('DATABASE_USERNAME', ''),
-            password: env('DATABASE_PASSWORD', ''),
-            ssl: {
-              rejectUnauthorized: false
-            }  
-          },
-          options: {
-            ssl: true,
-          },
-        },
-      },
-    }
-  }
+    }  
 }
